cdde99c4457ca3b63fcfdaad1a2760a2ae8d58fc
Paul Scala <89755484+pscala22@users.noreply.github.com>
2022-03-04 01:43:20 +0000

    - Asks user for sting -case 2 implementation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
-using namespace std:
+using namespace std;
 
 int main(){
 	int i, x;
 	char str[75];
+	
+	cout<<"Please enter a string:\t";
+	cin>>str;
 
 	cout<<"\nPlease chose from thr following options:\n";
 	cout<<"1. = Encrypt the string.\n";
@@ -18,7 +21,18 @@ int main(){
 				str[i]=str[i]+2; // the key for encryption is 3 that is added to the ASCII value. 	
 
 		cout << "\nEncrypted String: "<<str<<endl;
+			
 		break;
 
+		//second case for decrypting a string
+		case 2:
+			for (i=0; (i<75 && str[i]!='0');i++)
+			str[i]=str[i]-2;// the key for encryption is the 3 that is subtrated to the ASCII value.
+		cout << "\nDecrypted String: "<<str<< endl;
+
+		break;
+		default:
+			cout<<"\nInvalid Input\n";
 	}
+	return 0;
 }
